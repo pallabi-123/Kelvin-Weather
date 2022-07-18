62e05425defb569d621e1afeaa6c4dbf97f30792
Pallabi Dutta Bose <83059771+pallabi-123@users.noreply.github.com>
2022-07-18 11:00:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 // constant holding our Kelvin value
 const kelvin = 0;
+
 // Kelvin value converted to celcius
 const celsius = kelvin - 273; 
+
 // celsius value converted to fahrenheit
 let fahrenheit = celsius * (9/5) + 32;
+
 // round down the Fahrenheit temperature
 fahrenheit = Math.floor(fahrenheit);
 console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+
 let newton = celsius * (33/100);
 newton = Math.floor(newton);
 console.log(`The temperature is ${newton} degrees newton.`);
